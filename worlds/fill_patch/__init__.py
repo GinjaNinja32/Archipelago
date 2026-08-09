a0a7c31011e96198fdcc71e00f011cdfbc4756c8
Tabbomat <tabbe123@googlemail.com>
2026-08-09 15:32:44 +0200

    fix patch issue caused by module shadowing

--- worlds/fill_patch/__init__.py
+++ worlds/fill_patch/__init__.py
@@ -1,8 +1,8 @@
 import logging
 import sys
-import Fill
+import Fill as core_fill
 from worlds.AutoWorld import World
-from .Fill import fill_restrictive, remaining_fill
+from . import Fill as local_fill
 
 logger = logging.getLogger("FillPatch")
 
@@ -21,19 +21,19 @@ def apply_fill_patch():
     if _PATCH_APPLIED:
         return
 
-    original_fill_restrictive = getattr(Fill, "fill_restrictive", None)
-    original_remaining_fill = getattr(Fill, "remaining_fill", None)
+    original_fill_restrictive = getattr(core_fill, "fill_restrictive", None)
+    original_remaining_fill = getattr(core_fill, "remaining_fill", None)
 
-    Fill.fill_restrictive = fill_restrictive
-    Fill.remaining_fill = remaining_fill
+    core_fill.fill_restrictive = local_fill.fill_restrictive
+    core_fill.remaining_fill = local_fill.remaining_fill
 
     # update modules which used "from Fill import fill_restrictive"
     for mod_name, mod in sys.modules.items():
         if mod and mod_name != __name__:
             if getattr(mod, "fill_restrictive", None) is original_fill_restrictive:
-                setattr(mod, "fill_restrictive", fill_restrictive)
+                setattr(mod, "fill_restrictive", local_fill.fill_restrictive)
             if getattr(mod, "remaining_fill", None) is original_remaining_fill:
-                setattr(mod, "remaining_fill", remaining_fill)
+                setattr(mod, "remaining_fill", local_fill.remaining_fill)
 
     _PATCH_APPLIED = True
     logger.info("Fill algorithm patch applied.")
